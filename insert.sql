8a3a8ad1ac58bdf2d1ee6763cf7c5507bb97e287
mchart2905 <mael.charton--cautenet@edu.univ-fcomte.fr>
2026-01-30 07:40:08 +0100

    ajout des images

--- insert.sql
+++ insert.sql
@@ -38,26 +38,26 @@ INSERT INTO etat (id_etat, libelle) VALUES
 -- INSERTION DES PRODUITS (articles)
 
 INSERT INTO jean (id_jean, nom_jean, matiere, couleur, description, marque, photo, stock_, fournisseur, prix_jean, coupe_jean_id, taille_id) VALUES
-(1, 'Levi''s 501 Original', '100% Coton', 'Bleu Stone', 'Le jean iconique coupe droite boutonné.', 'Levi''s', 'img/levis.jpg', 50, 'Levi Strauss', 99.90, 2, 7),
-(2, 'Diesel Thommer', 'Coton/Elasthanne', 'Bleu moyen', 'Jean slim confortable et moderne.', 'Diesel', 'img/diesel.jpg', 25, 'Diesel Italy', 145.00, 1, 6),
-(3, 'Jack & Jones Glenn', 'Stretch', 'Gris Anthracite', 'Coupe près du corps, effet usé.', 'Jack & Jones', 'img/glenn.jpg', 100, 'Bestseller', 49.99, 3, 8),
-(4, 'Wrangler Texas', 'Coton', 'Bleu Brut', 'Jean robuste et authentique.', 'Wrangler', 'img/wrangler.jpg', 15, 'Wrangler Corp', 75.50, 2, 8),
-(5, 'Zara Man Basic', 'Denim Léger', 'Bleu Clair', 'Idéal pour l''été.', 'Zara', 'img/zara.jpg', 200, 'Inditex', 29.90, 3, 3),
-(6, 'G-Star Raw 3301', 'Denim Selvedge', 'Bleu Foncé', 'Inspiré des vêtements de travail.', 'G-Star', 'img/gstar.jpg', 10, 'G-Star Raw', 119.95, 5, 4),
-(7, 'Celio Powerflex', 'Super Stretch', 'Bleu Marine', 'Extrêmement élastique et confortable.', 'Celio', 'img/celio.jpg', 80, 'Celio France', 39.99, 1, 4),
-(8, 'Pepe Jeans Brighton', 'Coton', 'Bleu Foncé', 'Taille basse et jambes évasées.', 'Pepe Jeans', 'img/pepe_jeans.jpg', 20, 'Pepe Group', 85.00, 4, 2),
-(9, 'H&M Regular jeans', 'Coton Recyclé', 'Gris', 'Basique indispensable éco-responsable.', 'H&M', 'img/h&m.jpg', 150, 'H&M Group', 19.99, 2, 2),
-(10, 'Calvin Klein Jeans', 'Coton', 'Bleu Foncé', 'Look urbain et épuré.', 'Calvin Klein', 'img/ck.jpg', 30, 'PVH Corp', 110.00, 3, 1),
-(11, 'Lee Brooklyn Straight', 'Coton', 'Stone Wash', 'Coupe classique et intemporelle.', 'Lee', 'img/leebrooklyn.jpg', 45, 'Lee Jeans', 69.90, 5, 7),
-(12, 'Tommy Hilfiger Denton', 'Coton Bio', 'Beige', 'Chino style jean 5 poches.', 'Tommy Hilfiger', 'img/tommy.jpg', 35, 'PVH Corp', 99.00, 5, 6),
-(13, 'Petrol Industries Sherborne', 'Jeans', 'Bleu Moyen', 'Détails brodés sur les poches.', 'Petrol Industries', 'img/pi.jpg', 60, ' Petrol Industries US', 55.00, 2, 3),
-(14, 'Uniqlo Ezy Jean', 'Sweat/Denim', 'Bleu Moyen', 'L''apparence du jean, le confort du jogging.', 'Uniqlo', 'img/uniqlo.jpg', 90, 'Fast Retailing', 39.90, 1, 4),
-(15, 'Hugo Boss Delaware', 'Coton fin', 'Noir Profond', 'Jean habillé pour le bureau.', 'Hugo Boss', 'img/boss.jpg', 12, 'Hugo Boss AG', 159.00, 1, 7),
-(16, 'Pull&Bear Jean standard', 'Coton', 'Gris Clair', 'Coupe ample en haut, resserrée en bas.', 'Pull&Bear', 'img/pull_and_bear.jpg', 70, 'Inditex', 35.99, 2, 2),
-(17, 'Replay Anbass', 'Hyperflex', 'Bleu Foncé', 'Technologie stretch révolutionnaire.', 'Replay', 'img/replay.jpg', 18, 'Fashion Box', 135.00, 1, 5),
-(18, 'Superdry Officer', 'Coton épais', 'Kaki', 'Jean teinté style militaire.', 'Superdry', 'img/superdry.jpg', 40, 'Superdry PLC', 69.95, 2, 4),
-(19, 'Sullivan slim', '100% Coton', 'Bleu dixon ', 'Style années 90.', 'Ralf Lanren', 'img/rl.jpg', 55, 'Inditex', 35.99, 1, 3),
-(20, 'Jeans straight', 'Coton', 'Stone demin', 'Coupe mettant en valeur les formes.', 'Bonobo', 'img/bn.jpg', 22, 'Bonobo Inc', 95.00, 3, 2);
+(1, 'Levi''s 501 Original', '100% Coton', 'Bleu Stone', 'Le jean iconique coupe droite boutonné.', 'Levi''s', '/static/images/levis.jpg', 50, 'Levi Strauss', 99.90, 2, 7),
+(2, 'Diesel Thommer', 'Coton/Elasthanne', 'Bleu moyen', 'Jean slim confortable et moderne.', 'Diesel', '/static/images/diesel.jpg', 25, 'Diesel Italy', 145.00, 1, 6),
+(3, 'Jack & Jones Glenn', 'Stretch', 'Gris Anthracite', 'Coupe près du corps, effet usé.', 'Jack & Jones', '/static/images/glenn.jpg', 100, 'Bestseller', 49.99, 3, 8),
+(4, 'Wrangler Texas', 'Coton', 'Bleu Brut', 'Jean robuste et authentique.', 'Wrangler', '/static/images/wrangler.jpg', 15, 'Wrangler Corp', 75.50, 2, 8),
+(5, 'Zara Man Basic', 'Denim Léger', 'Bleu Clair', 'Idéal pour l''été.', 'Zara', '/static/images/zara.jpg', 200, 'Inditex', 29.90, 3, 3),
+(6, 'G-Star Raw 3301', 'Denim Selvedge', 'Bleu Foncé', 'Inspiré des vêtements de travail.', 'G-Star', '/static/images/gstar.jpg', 10, 'G-Star Raw', 119.95, 5, 4),
+(7, 'Celio Powerflex', 'Super Stretch', 'Bleu Marine', 'Extrêmement élastique et confortable.', 'Celio','/static/images/celio.jpg', 80, 'Celio France', 39.99, 1, 4),
+(8, 'Pepe Jeans Brighton', 'Coton', 'Bleu Foncé', 'Taille basse et jambes évasées.', 'Pepe Jeans', '/static/images/pepe_jeans.jpg', 20, 'Pepe Group', 85.00, 4, 2),
+(9, 'H&M Regular jeans', 'Coton Recyclé', 'Gris', 'Basique indispensable éco-responsable.', 'H&M', 'static/images/h&m.jpg', 150, 'H&M Group', 19.99, 2, 2),
+(10, 'Calvin Klein Jeans', 'Coton', 'Bleu Foncé', 'Look urbain et épuré.', 'Calvin Klein', '/static/images/ck.jpg', 30, 'PVH Corp', 110.00, 3, 1),
+(11, 'Lee Brooklyn Straight', 'Coton', 'Stone Wash', 'Coupe classique et intemporelle.', 'Lee', '/static/images/leebrooklyn.jpg', 45, 'Lee Jeans', 69.90, 5, 7),
+(12, 'Tommy Hilfiger Denton', 'Coton Bio', 'Beige', 'Chino style jean 5 poches.', 'Tommy Hilfiger', '/static/images/tommy.jpg', 35, 'PVH Corp', 99.00, 5, 6),
+(13, 'Petrol Industries Sherborne', 'Jeans', 'Bleu Moyen', 'Détails brodés sur les poches.', 'Petrol Industries', '/static/images/pi.jpg', 60, ' Petrol Industries US', 55.00, 2, 3),
+(14, 'Uniqlo Ezy Jean', 'Sweat/Denim', 'Bleu Moyen', 'L''apparence du jean, le confort du jogging.', 'Uniqlo', '/static/images/uniqlo.jpg', 90, 'Fast Retailing', 39.90, 1, 4),
+(15, 'Hugo Boss Delaware', 'Coton fin', 'Noir Profond', 'Jean habillé pour le bureau.', 'Hugo Boss', '/static/images/boss.jpg', 12, 'Hugo Boss AG', 159.00, 1, 7),
+(16, 'Pull&Bear Jean standard', 'Coton', 'Gris Clair', 'Coupe ample en haut, resserrée en bas.', 'Pull&Bear', '/static/images/pull_and_bear.jpg', 70, 'Inditex', 35.99, 2, 2),
+(17, 'Replay Anbass', 'Hyperflex', 'Bleu Foncé', 'Technologie stretch révolutionnaire.', 'Replay', '/static/images/replay.jpg', 18, 'Fashion Box', 135.00, 1, 5),
+(18, 'Superdry Officer', 'Coton épais', 'Kaki', 'Jean teinté style militaire.', 'Superdry', '/static/images/superdry.jpg', 40, 'Superdry PLC', 69.95, 2, 4),
+(19, 'Sullivan slim', '100% Coton', 'Bleu dixon ', 'Style années 90.', 'Ralf Lanren', '/static/images/rl.jpg', 55, 'Inditex', 35.99, 1, 3),
+(20, 'Jeans straight', 'Coton', 'Stone demin', 'Coupe mettant en valeur les formes.', 'Bonobo', '/static/images/bn.jpg', 22, 'Bonobo Inc', 95.00, 3, 2);
 
 -- Client 1 a une commande "Livrée"
 INSERT INTO commande (id_commande, date_achat, utilisateur_id, etat_id) VALUES
